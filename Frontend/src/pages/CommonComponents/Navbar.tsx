9bb9ab4361f53421a338462466595b6bf1220d69
seif7666 <seifeldin7666@gmail.com>
2023-12-10 08:26:05 +0200

    Extended navbar added!

--- Frontend/src/pages/CommonComponents/Navbar.tsx
+++ Frontend/src/pages/CommonComponents/Navbar.tsx
@@ -8,17 +8,15 @@ export default class Navbar extends React.Component {
 
   render(): React.ReactNode {
     return (
-      <div id="nav-div" className="container-fluid sticky-top">
-        <div className="row">
+      <nav id="nav-div" className="navbar container-fluid sticky-top">
+        <div className="row" style={{width:'100vw'}}>
           <div className="col-1 col-lg-2">
-            <Drawer/>
+            <Drawer />
           </div>
           <div id="nav-username-div" className="col-6 col-lg-6 text-start">
             <span>Welcome Username!</span>
           </div>
-          <div
-            className="col-5 offset-lg-2 col-lg-2"
-          >
+          <div className="col-5 offset-lg-2 col-lg-2">
             <div className="d-flex flex-row text-end">
               <div className="flex-fill">
                 <i className="bi bi-question-lg nav-icon"></i>
@@ -32,7 +30,7 @@ export default class Navbar extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
